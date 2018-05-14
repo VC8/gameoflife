aa0068c06c086608f5852bc3005f0acb0d8e66e1
vcassens <venance.cassens-tope@bonprix.net>
2018-05-15 01:08:39 +0200

    wip: - rename board to cells

--- src/main/java/de/cassens/gameoflife/Board.java
+++ src/main/java/de/cassens/gameoflife/Board.java
@@ -6,18 +6,18 @@ import java.util.stream.Collectors;
 
 public class Board {
 
-    private Cell[][] board;
+    private Cell[][] cells;
     private int generation;
     private final int maxRowPos;
     private final int maxColPos;
 
     public Board(int rows, int cols) {
         this.generation = 0;
-        this.board = new Cell[rows][cols];
+        this.cells = new Cell[rows][cols];
 
         for (int row = 0; row < rows; row++) {
             for (int col = 0; col < cols; col++) {
-                boolean isAlive = Math.random() > 0.4;
+                boolean isAlive = Math.random() < 0.4;
                 this.cells[row][col] = new Cell(row, col, isAlive);
             }
         }
@@ -28,14 +28,14 @@ public class Board {
 
     public void printBoard() {
         System.out.println("----- print generation: " + this.generation + " -----");
-        Arrays.stream(this.board).forEach(row -> {
+        Arrays.stream(this.cells).forEach(row -> {
             Arrays.stream(row).forEach(System.out::print);
             System.out.println();
         });
     }
 
     public void nextGeneration() {
-        List<Cell[]> newBoard = Arrays.stream(this.board).map(row -> {
+        List<Cell[]> newCells = Arrays.stream(this.cells).map(row -> {
             List<Cell> rowCellsList = Arrays.stream(row).map(cell -> {
                 int livingNeighborCount = getLivingNeighborCountForCell(cell);
 
@@ -59,8 +59,8 @@ public class Board {
             return rowCellsArray;
         }).collect(Collectors.toList());
 
-        // update game board
-        newBoard.toArray(this.board);
+        // update game cells
+        newCells.toArray(this.cells);
 
         this.generation++;
     }
@@ -72,25 +72,25 @@ public class Board {
 
         // prepare neighbor positions
         int leftCol = colPos - 1;
-        if (leftCol < 0) leftCol = this.maxColPos; // if the left neighbors are on the other side of the board
+        if (leftCol < 0) leftCol = this.maxColPos; // if the left neighbors are on the other side of the cells
         int rightCol = colPos + 1;
-        if (rightCol > this.maxColPos) rightCol = 0; // if the right neighbors are on the other side of the board
+        if (rightCol > this.maxColPos) rightCol = 0; // if the right neighbors are on the other side of the cells
 
         int rowAbove = rowPos - 1;
-        if (rowAbove < 0) rowAbove = this.maxRowPos; // if the neighbors above are on the other side of the board
+        if (rowAbove < 0) rowAbove = this.maxRowPos; // if the neighbors above are on the other side of the cells
         int rowBelow = rowPos + 1;
-        if (rowBelow > this.maxRowPos) rowBelow = 0; // if the neighbors below are on the other side of the board
+        if (rowBelow > this.maxRowPos) rowBelow = 0; // if the neighbors below are on the other side of the cells
 
         // add status of neighbors to array
         int livingNeighborCount = 0;
-        livingNeighborCount += this.board[rowPos][leftCol].isAlive() ? 1 : 0;
-        livingNeighborCount += this.board[rowAbove][leftCol].isAlive() ? 1 : 0;
-        livingNeighborCount += this.board[rowAbove][colPos].isAlive() ? 1 : 0;
-        livingNeighborCount += this.board[rowAbove][rowAbove].isAlive() ? 1 : 0;
-        livingNeighborCount += this.board[rowPos][rightCol].isAlive() ? 1 : 0;
-        livingNeighborCount += this.board[rowBelow][rightCol].isAlive() ? 1 : 0;
-        livingNeighborCount += this.board[rowBelow][colPos].isAlive() ? 1 : 0;
-        livingNeighborCount += this.board[rowBelow][leftCol].isAlive() ? 1 : 0;
+        livingNeighborCount += this.cells[rowPos][leftCol].isAlive() ? 1 : 0;
+        livingNeighborCount += this.cells[rowAbove][leftCol].isAlive() ? 1 : 0;
+        livingNeighborCount += this.cells[rowAbove][colPos].isAlive() ? 1 : 0;
+        livingNeighborCount += this.cells[rowAbove][rowAbove].isAlive() ? 1 : 0;
+        livingNeighborCount += this.cells[rowPos][rightCol].isAlive() ? 1 : 0;
+        livingNeighborCount += this.cells[rowBelow][rightCol].isAlive() ? 1 : 0;
+        livingNeighborCount += this.cells[rowBelow][colPos].isAlive() ? 1 : 0;
+        livingNeighborCount += this.cells[rowBelow][leftCol].isAlive() ? 1 : 0;
 
         return livingNeighborCount;
     }
