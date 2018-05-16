72889a3e4312ab0a49355494785dfa319e46f5af
whitehatracoon <venance.cassens@whitehatracoons.de>
2018-05-16 23:01:35 +0200

    merged board printer into master

--- src/main/java/de/cassens/gameoflife/core/boardprinter/BoardPrinter.java
+++ src/main/java/de/cassens/gameoflife/core/boardprinter/BoardPrinter.java
@@ -1,15 +1,23 @@
 package de.cassens.gameoflife.core.boardprinter;
 
 import de.cassens.gameoflife.model.cell.Cell;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Marker;
+import org.slf4j.MarkerFactory;
 
 import java.util.Arrays;
 
+// TODO delete after building frontend
 public class BoardPrinter {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(BoardPrinter.class);
+
     public void printBoard(int generation, Cell[][] board) {
-        System.out.println("----- print generation: " + generation + " -----");
+        System.out.println("----- print generation: "+generation+" -----");
+
         Arrays.stream(board).forEach(row -> {
-            Arrays.stream(row).forEach(System.out::print);
+            Arrays.stream(row).forEach(System.out::println);
             System.out.println();
         });
     }
