53af7e28e0858f102a23ee9e85482de44d832459
whitehatracoon <venance.cassens@whitehatracoons.de>
2018-05-27 02:48:16 +0200

    spring profiles + jar image:1.0.1-SNAPSHOT + added image to docker compose

--- src/main/java/de/cassens/gameoflife/TheGameOfLifeApplication.java
+++ src/main/java/de/cassens/gameoflife/TheGameOfLifeApplication.java
@@ -1,6 +1,8 @@
 package de.cassens.gameoflife;
 
 import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
@@ -10,13 +12,18 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 @EnableMongoRepositories
 public class TheGameOfLifeApplication extends AbstractMongoConfiguration {
 
+    @Value("${spring.data.mongodb.host}")
+    private String mongoDbHost;
+
     public static void main(String[] args) {
         SpringApplication.run(TheGameOfLifeApplication.class, args);
     }
 
     @Override
     public MongoClient mongoClient() {
-        return new MongoClient();
+        return new MongoClient(
+                new MongoClientURI("mongodb://" + mongoDbHost + "/data")
+        );
     }
 
     @Override
