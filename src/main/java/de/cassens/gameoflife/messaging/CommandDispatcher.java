3c987f2c142fe87d34917316db79ef64764671c5
vcassens <venance.cassens-tope@bonprix.net>
2018-06-06 23:43:54 +0200

    CommandDispatcher

--- src/main/java/de/cassens/gameoflife/messaging/CommandDispatcher.java
+++ src/main/java/de/cassens/gameoflife/messaging/CommandDispatcher.java
@@ -1,30 +1,47 @@
-// TODO CommandDispatcher
-//package de.cassens.gameoflife.messaging;
-//
-//import de.cassens.gameoflife.board.service.increment.IncrementBoardCycleService;
-//import de.cassens.gameoflife.messaging.model.type.CommandType;
-//import de.cassens.gameoflife.messaging.model.message.Message;
-//import de.cassens.gameoflife.messaging.model.type.MessageType;
-//import org.springframework.stereotype.Component;
-//
-//@Component
-//public class CommandDispatcher {
-//
-//    private final IncrementBoardCycleService incrementBoardCycleService;
-//
-//    public CommandDispatcher(IncrementBoardCycleService incrementBoardCycleService) {
-//        this.incrementBoardCycleService = incrementBoardCycleService;
-//    }
-//
-//    public void dispatchCommandFromMessage(Message<CommandType> message) {
-//        final MessageType messageType = message.getMessageType();
-//
-//        if (MessageType.COMMAND.equals(messageType)) {
-//            final CommandType commandType = message.getPayload();
-//
-//            if (CommandType.INCREMENT.equals(commandType)) {
-//                incrementBoardCycleService.incrementBoardCycle();
-//            }
-//        }
-//    }
-//}
+package de.cassens.gameoflife.messaging;
+
+import de.cassens.gameoflife.board.model.payload.BoardPayload;
+import de.cassens.gameoflife.board.service.create.CreateBoardService;
+import de.cassens.gameoflife.board.service.decrement.DecrementBoardCycleService;
+import de.cassens.gameoflife.board.service.increment.IncrementBoardCycleService;
+import de.cassens.gameoflife.messaging.model.message.CommandMessage;
+import de.cassens.gameoflife.messaging.model.type.CommandType;
+import de.cassens.gameoflife.messaging.model.message.Message;
+import de.cassens.gameoflife.messaging.model.type.MessageType;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommandDispatcher {
+
+    private final CreateBoardService createBoardService;
+    private final IncrementBoardCycleService incrementBoardCycleService;
+    private final DecrementBoardCycleService decrementBoardCycleService;
+
+    public CommandDispatcher(CreateBoardService createBoardService,
+                             IncrementBoardCycleService incrementBoardCycleService,
+                             DecrementBoardCycleService decrementBoardCycleService) {
+        this.createBoardService = createBoardService;
+        this.incrementBoardCycleService = incrementBoardCycleService;
+        this.decrementBoardCycleService = decrementBoardCycleService;
+    }
+
+    public void dispatchCommandFromMessage(Message message) {
+        final MessageType messageType = message.getMessageType();
+
+        if (MessageType.COMMAND.equals(messageType)) {
+            final CommandMessage commandMessage = (CommandMessage) message;
+            final CommandType commandType = commandMessage.getCommandType();
+
+            if (CommandType.INCREMENT.equals(commandType)) {
+                incrementBoardCycleService.incrementBoardCycle();
+            }
+            if (CommandType.DECREMENT.equals(commandType)) {
+                decrementBoardCycleService.decrementBoardCycle();
+            }
+            if (CommandType.CREATE.equals(commandType)) {
+                final BoardPayload boardPayload = commandMessage.getPayload();
+                createBoardService.createBoard(boardPayload.getRows(), boardPayload.getCols());
+            }
+        }
+    }
+}
--- src/test/java/de/cassens/gameoflife/messaging/CommandDispatcherTest.java
+++ src/test/java/de/cassens/gameoflife/messaging/CommandDispatcherTest.java
@@ -1,36 +1,95 @@
-// TODO CommandDispatcherTest
+package de.cassens.gameoflife.messaging;
 
-//package de.cassens.gameoflife.messaging;
-//
-//import de.cassens.gameoflife.board.service.increment.IncrementBoardCycleService;
-//import de.cassens.gameoflife.messaging.model.type.CommandType;
-//import de.cassens.gameoflife.messaging.model.message.Message;
-//import de.cassens.gameoflife.messaging.model.type.MessageType;
-//import org.junit.Test;
-//
-//import static org.hamcrest.MatcherAssert.assertThat;
-//import static org.hamcrest.core.Is.is;
-//import static org.mockito.Mockito.mock;
-//import static org.mockito.Mockito.verify;
-//import static org.mockito.Mockito.when;
-//
-//public class CommandDispatcherTest {
-//
-//    private final IncrementBoardCycleService incrementBoardCycleService = mock(IncrementBoardCycleService.class);
-//    private final CommandDispatcher commandDispatcher = new CommandDispatcher(incrementBoardCycleService);
-//
-//    @SuppressWarnings("unchecked")
-//    @Test
-//    public void shouldDispatchIncrementBoardCommand() {
-//        // given
-//        Message<CommandType> message = mock(Message.class);
-//        when(message.getMessageType()).thenReturn(MessageType.COMMAND);
-//        when(message.getPayload()).thenReturn(CommandType.INCREMENT);
-//
-//        // when
-//        commandDispatcher.dispatchCommandFromMessage(message);
-//
-//        // then
-//        verify(incrementBoardCycleService).incrementBoardCycle();
-//    }
-//}
+import de.cassens.gameoflife.board.model.payload.BoardPayload;
+import de.cassens.gameoflife.board.service.create.CreateBoardService;
+import de.cassens.gameoflife.board.service.decrement.DecrementBoardCycleService;
+import de.cassens.gameoflife.board.service.increment.IncrementBoardCycleService;
+import de.cassens.gameoflife.messaging.model.message.CommandMessage;
+import de.cassens.gameoflife.messaging.model.message.EventMessage;
+import de.cassens.gameoflife.messaging.model.message.MessageFactory;
+import de.cassens.gameoflife.messaging.model.type.CommandType;
+import de.cassens.gameoflife.messaging.model.type.EventType;
+import de.cassens.gameoflife.messaging.model.type.MessageType;
+import org.junit.Test;
+
+import static org.mockito.Mockito.*;
+
+public class CommandDispatcherTest {
+
+    private final CreateBoardService createBoardService = mock(CreateBoardService.class);
+    private final IncrementBoardCycleService incrementBoardCycleService = mock(IncrementBoardCycleService.class);
+    private final DecrementBoardCycleService decrementBoardCycleService = mock(DecrementBoardCycleService.class);
+    private final CommandDispatcher commandDispatcher = new CommandDispatcher(createBoardService,
+                                                                              incrementBoardCycleService,
+                                                                              decrementBoardCycleService);
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void shouldDispatchIncrementBoardCommand() {
+        // given
+        CommandMessage message = givenCommandMessage(CommandType.INCREMENT);
+
+        // when
+        commandDispatcher.dispatchCommandFromMessage(message);
+
+        // then
+        verify(incrementBoardCycleService).incrementBoardCycle();
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void shouldDispatchDecrementBoardCommand() {
+        // given
+        CommandMessage message = givenCommandMessage(CommandType.DECREMENT);
+
+        // when
+        commandDispatcher.dispatchCommandFromMessage(message);
+
+        // then
+        verify(decrementBoardCycleService).decrementBoardCycle();
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void shouldDispatchCreateBoardCommand() {
+        // given
+        CommandMessage message = givenCommandMessage(CommandType.CREATE);
+        final int count = 2;
+        givenMessagePayload(message,count);
+
+        // when
+        commandDispatcher.dispatchCommandFromMessage(message);
+
+        // then
+        verify(createBoardService).createBoard(count, count);
+    }
+
+    @Test
+    public void shouldDoNothingWhenMessageTypeIsNotCommand() {
+        // given
+        MessageFactory messageFactory = new MessageFactory();
+        final EventMessage eventMessage = messageFactory.createEventMessage(EventType.CREATED);
+
+        // when
+        commandDispatcher.dispatchCommandFromMessage(eventMessage);
+
+        // then
+        verify(createBoardService, never()).createBoard(anyInt(), anyInt());
+        verify(incrementBoardCycleService, never()).incrementBoardCycle();
+        verify(decrementBoardCycleService, never()).decrementBoardCycle();
+    }
+
+    private CommandMessage givenCommandMessage(CommandType commandType) {
+        CommandMessage message = mock(CommandMessage.class);
+        when(message.getMessageType()).thenReturn(MessageType.COMMAND);
+        when(message.getCommandType()).thenReturn(commandType);
+        return message;
+    }
+
+    private void givenMessagePayload(CommandMessage message, int count) {
+        final BoardPayload payload = new BoardPayload();
+        payload.setRows(count);
+        payload.setCols(count);
+        when(message.getPayload()).thenReturn(payload);
+    }
+}
