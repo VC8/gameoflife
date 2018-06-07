49e7ea3b9449d13c24254cd3a6f6a64a248c53dc
vcassens <venance.cassens-tope@bonprix.net>
2018-06-07 17:51:01 +0200

    wip: MessageSender

--- src/main/java/de/cassens/gameoflife/messaging/MessageSender.java
+++ src/main/java/de/cassens/gameoflife/messaging/MessageSender.java
@@ -1,54 +1,54 @@
-// TODO MessageSender
-//package de.cassens.gameoflife.messaging;
-//
-//import com.rabbitmq.client.Channel;
-//import de.cassens.gameoflife.board.model.Board;
-//import de.cassens.gameoflife.messaging.config.Queues;
-//import de.cassens.gameoflife.messaging.model.type.EventType;
-//import de.cassens.gameoflife.messaging.model.message.Message;
-//import de.cassens.gameoflife.messaging.model.message.MessageFactory;
-//import org.springframework.stereotype.Component;
-//
-//import java.io.IOException;
-//
-//@Component
-//public class MessageSender {
-//
-//    private final Channel channel;
-//    private final MessageFactory messageFactory;
-//    private final MessageConverter messageConverter;
-//
-//    public MessageSender(Channel channel,
-//                         MessageFactory messageFactory,
-//                         MessageConverter messageConverter) {
-//        this.channel = channel;
-//        this.messageFactory = messageFactory;
-//        this.messageConverter = messageConverter;
-//    }
-//
-//    public final void sendEventMessage(EventType eventType) throws IOException {
-//        final Message<EventType> eventMessage = messageFactory.createEventMessage(eventType);
-//        final String eventMessageJson = messageConverter.convertToJsonString(eventMessage);
-//
-//        publishMessage(eventMessageJson);
-//
-//        logMessage(eventMessageJson);
-//    }
-//
-//    public final void sendDocumentMessage(Board board) throws IOException {
-//        final Message<Board> documentMessage = messageFactory.createDocumentMessage(board);
-//        final String documentMessageJson = messageConverter.convertToJsonString(documentMessage);
-//
-//        publishMessage(documentMessageJson);
-//
-//        logMessage(documentMessageJson);
-//    }
-//
-//    private void publishMessage(String messageJson) throws IOException {
-//        this.channel.basicPublish("", Queues.BOARD_EVENTS.getQueueName(), null, messageJson.getBytes());
-//    }
-//
-//    private void logMessage(String messageJson) {
-//        System.out.println("Sent '" + messageJson + "'");
-//    }
-//}
+package de.cassens.gameoflife.messaging;
+
+import com.rabbitmq.client.Channel;
+import de.cassens.gameoflife.board.model.Board;
+import de.cassens.gameoflife.messaging.config.Queues;
+import de.cassens.gameoflife.messaging.model.message.DocumentMessage;
+import de.cassens.gameoflife.messaging.model.message.EventMessage;
+import de.cassens.gameoflife.messaging.model.message.MessageFactory;
+import de.cassens.gameoflife.messaging.model.type.EventType;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class MessageSender {
+
+    private final Channel channel;
+    private final MessageFactory messageFactory;
+    private final MessageConverter messageConverter;
+
+    public MessageSender(Channel channel,
+                         MessageFactory messageFactory,
+                         MessageConverter messageConverter) {
+        this.channel = channel;
+        this.messageFactory = messageFactory;
+        this.messageConverter = messageConverter;
+    }
+
+    public void sendEventMessage(EventType eventType) throws IOException {
+        final EventMessage eventMessage = messageFactory.createEventMessage(eventType);
+        final String eventMessageJson = messageConverter.convertToJsonString(eventMessage);
+
+        publishMessage(eventMessageJson);
+
+        logMessage(eventMessageJson);
+    }
+
+    public void sendDocumentMessage(Board board) throws IOException {
+        final DocumentMessage<Board> documentMessage = messageFactory.createDocumentMessage(board);
+        final String documentMessageJson = messageConverter.convertToJsonString(documentMessage);
+
+        publishMessage(documentMessageJson);
+
+        logMessage(documentMessageJson);
+    }
+
+    private void publishMessage(String messageJson) throws IOException {
+        this.channel.basicPublish("", Queues.BOARD_EVENTS.getQueueName(), null, messageJson.getBytes());
+    }
+
+    private void logMessage(String messageJson) {
+        System.out.println("Sent '" + messageJson + "'");
+    }
+}
--- src/test/java/de/cassens/gameoflife/messaging/MessageSenderTest.java
+++ src/test/java/de/cassens/gameoflife/messaging/MessageSenderTest.java
@@ -1,108 +1,99 @@
-// TODO MessageSenderTest
+package de.cassens.gameoflife.messaging;
 
-//package de.cassens.gameoflife.messaging;
-//
-//import com.rabbitmq.client.Channel;
-//import de.cassens.gameoflife.board.model.Board;
-//import de.cassens.gameoflife.cell.model.Cell;
-//import de.cassens.gameoflife.messaging.model.type.EventType;
-//import de.cassens.gameoflife.messaging.model.message.Message;
-//import de.cassens.gameoflife.messaging.model.message.MessageFactory;
-//import de.cassens.gameoflife.testUtil.TestBoardFactory;
-//import org.junit.Test;
-//
-//import java.io.IOException;
-//import java.nio.file.Files;
-//import java.nio.file.Paths;
-//import java.util.Optional;
-//import java.util.stream.Stream;
-//
-//import static org.hamcrest.MatcherAssert.assertThat;
-//import static org.hamcrest.core.Is.is;
-//import static org.hamcrest.core.IsNull.notNullValue;
-//import static org.mockito.ArgumentMatchers.any;
-//import static org.mockito.ArgumentMatchers.eq;
-//import static org.mockito.ArgumentMatchers.isNull;
-//import static org.mockito.Mockito.mock;
-//import static org.mockito.Mockito.verify;
-//import static org.mockito.Mockito.when;
-//
-//public class MessageSenderTest {
-//
-//    private final Channel channel = mock(Channel.class);
-//    private final MessageFactory messageFactory = mock(MessageFactory.class);
-//    private final MessageConverter messageConverter = mock(MessageConverter.class);
-//    private final MessageSender messageSender = new MessageSender(channel, messageFactory, messageConverter);
-//
-//    @SuppressWarnings("unchecked")
-//    @Test
-//    public void shouldSendCreatedEventMessage() throws IOException {
-//        // given
-//        final Message message = mock(Message.class);
-//        when(messageFactory.createEventMessage(EventType.CREATED)).thenReturn(message);
-//        final String eventJson = "{\"messageType\":\"EVENT\",\"payload\":\"CREATED\"}";
-//        when(messageConverter.convertToJsonString(message)).thenReturn(eventJson);
-//
-//        // when
-//        messageSender.sendEventMessage(EventType.CREATED);
-//
-//        // then
-//        verify(channel).basicPublish("", "BOARD_EVENTS", null, eventJson.getBytes());
-//    }
-//
-//    @SuppressWarnings("unchecked")
-//    @Test
-//    public void shouldSendIncrementedEventMessage() throws IOException {
-//        // given
-//        final Message message = mock(Message.class);
-//        when(messageFactory.createEventMessage(EventType.INCREMENTED)).thenReturn(message);
-//        final String eventJson = "{\"messageType\":\"EVENT\",\"payload\":\"INCREMENTED\"}";
-//        when(messageConverter.convertToJsonString(message)).thenReturn(eventJson);
-//
-//        // when
-//        messageSender.sendEventMessage(EventType.INCREMENTED);
-//
-//        // then
-//        verify(channel).basicPublish("", "BOARD_EVENTS", null, eventJson.getBytes());
-//    }
-//
-//    @SuppressWarnings("unchecked")
-//    @Test
-//    public void shouldSendDecrementedEventMessage() throws IOException {
-//        // given
-//        final Message message = mock(Message.class);
-//        when(messageFactory.createEventMessage(EventType.DECREMENTED)).thenReturn(message);
-//        final String eventJson = "{\"messageType\":\"EVENT\",\"payload\":\"DECREMENTED\"}";
-//        when(messageConverter.convertToJsonString(message)).thenReturn(eventJson);
-//
-//        // when
-//        messageSender.sendEventMessage(EventType.DECREMENTED);
-//
-//        // then
-//        verify(channel).basicPublish("", "BOARD_EVENTS", null, eventJson.getBytes());
-//    }
-//
-//    @SuppressWarnings("unchecked")
-//    @Test
-//    public void shouldSendBoardStateMessage() throws IOException {
-//        // given
-//        final Cell[][] cells = TestBoardFactory.createBoard();
-//        final Board board = new Board(cells, 2);
-//        final Message message = mock(Message.class);
-//        when(messageFactory.createDocumentMessage(board)).thenReturn(message);
-//        final String documentJson = getJson("document-message.json");
-//        when(messageConverter.convertToJsonString(message)).thenReturn(documentJson);
-//
-//        // when
-//        messageSender.sendDocumentMessage(board);
-//
-//        // then
-//        verify(channel).basicPublish("", "BOARD_EVENTS", null, documentJson.getBytes());
-//    }
-//
-//    private String getJson(String file) throws IOException {
-//        final Stream<String> lines = Files.lines(Paths.get("src/test/resources/" + file));
-//        final Optional<String> optional = lines.findFirst();
-//        return optional.orElse("");
-//    }
-//}
+import com.rabbitmq.client.Channel;
+import de.cassens.gameoflife.board.model.Board;
+import de.cassens.gameoflife.cell.model.Cell;
+import de.cassens.gameoflife.messaging.model.message.DocumentMessage;
+import de.cassens.gameoflife.messaging.model.message.EventMessage;
+import de.cassens.gameoflife.messaging.model.message.MessageFactory;
+import de.cassens.gameoflife.messaging.model.type.EventType;
+import de.cassens.gameoflife.testUtil.TestBoardFactory;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import static org.mockito.Mockito.*;
+
+public class MessageSenderTest {
+
+    private final Channel channel = mock(Channel.class);
+    private final MessageFactory messageFactory = mock(MessageFactory.class);
+    private final MessageConverter messageConverter = mock(MessageConverter.class);
+    private final MessageSender messageSender = new MessageSender(channel, messageFactory, messageConverter);
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void shouldSendCreatedEventMessage() throws IOException {
+        // given
+        final EventMessage eventMessage = mock(EventMessage.class);
+        when(messageFactory.createEventMessage(EventType.CREATED)).thenReturn(eventMessage);
+        final String eventJson = "{\"messageType\":\"EVENT\",\"payload\":\"CREATED\"}";
+        when(messageConverter.convertToJsonString(eventMessage)).thenReturn(eventJson);
+
+        // when
+        messageSender.sendEventMessage(EventType.CREATED);
+
+        // then
+        verify(channel).basicPublish("", "BOARD_EVENTS", null, eventJson.getBytes());
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void shouldSendIncrementedEventMessage() throws IOException {
+        // given
+        final EventMessage eventMessage = mock(EventMessage.class);
+        when(messageFactory.createEventMessage(EventType.INCREMENTED)).thenReturn(eventMessage);
+        final String eventJson = "{\"messageType\":\"EVENT\",\"payload\":\"INCREMENTED\"}";
+        when(messageConverter.convertToJsonString(eventMessage)).thenReturn(eventJson);
+
+        // when
+        messageSender.sendEventMessage(EventType.INCREMENTED);
+
+        // then
+        verify(channel).basicPublish("", "BOARD_EVENTS", null, eventJson.getBytes());
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void shouldSendDecrementedEventMessage() throws IOException {
+        // given
+        final EventMessage eventMessage = mock(EventMessage.class);
+        when(messageFactory.createEventMessage(EventType.DECREMENTED)).thenReturn(eventMessage);
+        final String eventJson = "{\"messageType\":\"EVENT\",\"payload\":\"DECREMENTED\"}";
+        when(messageConverter.convertToJsonString(eventMessage)).thenReturn(eventJson);
+
+        // when
+        messageSender.sendEventMessage(EventType.DECREMENTED);
+
+        // then
+        verify(channel).basicPublish("", "BOARD_EVENTS", null, eventJson.getBytes());
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void shouldSendBoardStateMessage() throws IOException {
+        // given
+        final Cell[][] cells = TestBoardFactory.createBoard();
+        final Board board = new Board(cells, 2);
+        final DocumentMessage documentMessage = mock(DocumentMessage.class);
+        when(messageFactory.createDocumentMessage(board)).thenReturn(documentMessage);
+        final String documentJson = getJson("document-message.json");
+        when(messageConverter.convertToJsonString(documentMessage)).thenReturn(documentJson);
+
+        // when
+        messageSender.sendDocumentMessage(board);
+
+        // then
+        verify(channel).basicPublish("", "BOARD_EVENTS", null, documentJson.getBytes());
+    }
+
+    private String getJson(String file) throws IOException {
+        final Stream<String> lines = Files.lines(Paths.get("src/test/resources/" + file));
+        final Optional<String> optional = lines.findFirst();
+        return optional.orElse("");
+    }
+}
